2cb7735a9447de2062eef2407246a694167fbfa9
Tony Eder <tonyeder11@gmail.com>
2024-03-01 14:31:14 -0800

    started 2:15 - 10% now. Working on instantiation of all classes. Will create attributes within each class to hold relation with other classes, etc.

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,6 +2,16 @@ class NationalPark:
 
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, "name"):
+            if isinstance(name, str) and 3 < len(name):
+                self._name = name
         
     def trips(self):
         pass
@@ -16,19 +26,19 @@ class NationalPark:
         pass
 
 
-class Trip:
-    
-    def __init__(self, visitor, national_park, start_date, end_date):
-        self.visitor = visitor
-        self.national_park = national_park
-        self.start_date = start_date
-        self.end_date = end_date
-
-
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 < len(name) < 15:
+            self._name = name
         
     def trips(self):
         pass
@@ -38,3 +48,23 @@ class Visitor:
     
     def total_visits_at_park(self, park):
         pass
+
+
+class Trip:
+    
+    def __init__(self, visitor, national_park, start_date, end_date):
+        self.visitor = visitor
+        self.national_park = national_park
+        self.start_date = start_date
+        self.end_date = end_date
+    
+    @property
+    def start_date(self):
+        self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date.strftime("%B %dth")
+
+    
---
+++ lib/classes/test.py
@@ -0,0 +1,4 @@
+from many_to_many import NationalPark
+from many_to_many import Visitor
+from many_to_many import Trip
+
