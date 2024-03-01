8fa1fc84622d0924e6a9eac513be0f0e5cdc298e
Tony Eder <tonyeder11@gmail.com>
2024-03-01 14:50:59 -0800

    2:50 - 17% - national_park has many trips completed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -22,7 +22,7 @@ class NationalPark:
                 self._name = name
         
     def trips(self):
-        pass
+        return [ trip for trip in Trip.all if trip.national_park == self ]
     
     def visitors(self):
         pass
@@ -55,7 +55,7 @@ class Visitor:
             self._name = name
         
     def trips(self):
-        pass
+        return [ trip for trip in Trip.all if trip.visitor == self ]
     
     def national_parks(self):
         pass
@@ -78,12 +78,12 @@ class Trip:
         Trip.all.append(self)
 
         # join to NationalPark
-        self.NationalPark._visitors.append(self.visitor)
-        self.NationalPark._trips.append(self)
+        self.national_park._visitors.append(self.visitor)
+        self.national_park._trips.append(self)
 
         # join to Visitors
-        self.Visitor._national_parks.append(self.national_park)
-        self.Visitor._trips.append(self)
+        self.visitor._national_parks.append(self.national_park)
+        self.visitor._trips.append(self)
 
     @property
     def start_date(self):
@@ -92,7 +92,7 @@ class Trip:
     @start_date.setter
     def start_date(self, start_date):
         if isinstance(start_date, str) and len(start_date) >= 7:
-            self._start_date = start_date.strftime("%B %dth")
+            self._start_date = start_date
 
     @property
     def end_date(self):
@@ -101,4 +101,22 @@ class Trip:
     @end_date.setter
     def end_date(self, end_date):
         if isinstance(end_date, str) and len(end_date) >= 7:
-            self._end_date = end_date.strftime("%B %dth")
+            self._end_date = end_date
+    
+    @property
+    def visitor(self):
+        return self._visitor
+
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+
+    @property 
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
