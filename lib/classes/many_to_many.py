0737bf59f03a43c81dc1a2a02923a4dd414bf8b6
Tony Eder <tonyeder11@gmail.com>
2024-03-01 14:41:13 -0800

    2:39 - all class initializers and properties have been added. Appending class objects via intermediary class Trip.

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,15 @@
+import datetime
+
 class NationalPark:
 
     def __init__(self, name):
         self.name = name
+
+        # attribute for visitors
+        self._visitors = []
+
+        # attribute for trips
+        self._trips = []
     
     @property
     def name(self):
@@ -31,6 +39,12 @@ class Visitor:
     def __init__(self, name):
         self.name = name
 
+        # attribute for national parks
+        self._national_parks = []
+
+        # attribute for trips
+        self._trips = []
+
     @property
     def name(self):
         self._name
@@ -51,13 +65,26 @@ class Visitor:
 
 
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
-    
+
+        # store all trips
+        Trip.all.append(self)
+
+        # join to NationalPark
+        self.NationalPark._visitors.append(self.visitor)
+        self.NationalPark._trips.append(self)
+
+        # join to Visitors
+        self.Visitor._national_parks.append(self.national_park)
+        self.Visitor._trips.append(self)
+
     @property
     def start_date(self):
         self._start_date
@@ -67,4 +94,11 @@ class Trip:
         if isinstance(start_date, str) and len(start_date) >= 7:
             self._start_date = start_date.strftime("%B %dth")
 
-    
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date.strftime("%B %dth")
